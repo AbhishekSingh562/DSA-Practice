6f8ed3bd4c2fb6b671448875f99b664dd5397741
abhishek <abhisingh562002offc@gmail.com>
2023-11-06 10:34:59 +0530

    completed the tree data structure

--- DataStructures/BinaryTree/Tree.java
+++ DataStructures/BinaryTree/Tree.java
@@ -4,23 +4,37 @@ public class Tree {
     static Scanner sc = null;
     public static void main(String[] args) {
         sc = new Scanner(System.in);
-        createTree();
+        Node root = createTree();
+        inOrder(root);
+        preOrder(root);
+        postOrder(root);
+        int height = getHeight(root);
+        System.out.println("Height of the binary tree is "+height);
+        int sizeOfBT = sizeOfBinaryTree(root);
+        System.out.println("Size of the binary tree is "+sizeOfBT);
+        int max = getMaxOfBT(root);
+        System.out.println("Maximum value present in this binary tree is "+ max);
+        int min = getMinOfBT(root);
+        System.out.println("Minimum value present in this binary tree is "+ min);
     }
     static Node createTree(){
         Node root = null;
-        System.out.println("Enter the data: ");
+        System.out.print("Enter the data: ");
         int data = sc.nextInt();
 
         if(data == -1) return null;
         root = new Node(data);
 
-        System.out.println("Enter left for "+ data);
+        System.out.println();
+        System.out.print("Enter left for "+ data);
         root.left = createTree();
 
-        System.out.println("Enter right for "+ data);
+        System.out.println();
+        System.out.print("Enter right for "+ data);
         root.right = createTree();
 
         return root;
+
     }
     static void inOrder(Node root){
         if(root == null) return;
@@ -43,6 +57,38 @@ public class Tree {
         postOrder(root.right);
         System.out.print(root.data+ " ");
     }
+    static int getHeight(Node root) {
+        if (root == null) {
+            return 0; // Height of an empty tree is 0.
+        } else {
+            int leftHeight = getHeight(root.left);
+            int rightHeight = getHeight(root.right);
+            return Math.max(leftHeight, rightHeight) + 1; // Add 1 to account for the current node.
+        }
+    }
+
+    static int sizeOfBinaryTree(Node root){
+        if(root == null) return 0;
+        else return sizeOfBinaryTree(root.left)+sizeOfBinaryTree(root.right)+1;
+    }
+
+    static int getMaxOfBT(Node root){
+        if(root == null) return Integer.MIN_VALUE;
+
+        else return Math.max(root.data, Math.max(getMaxOfBT(root.left),getMaxOfBT(root.right)));
+    }
+
+    static int getMinOfBT(Node root) {
+        if (root == null) {
+            return Integer.MAX_VALUE; // Return the maximum possible integer value to indicate an empty tree.
+        } else {
+            int leftMin = getMinOfBT(root.left);
+            int rightMin = getMinOfBT(root.right);
+            return Math.min(root.data, Math.min(leftMin, rightMin));
+        }
+    }
+
+
 }
 
 class Node{
